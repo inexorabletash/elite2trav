a705efc33cd04d84f45029729a2a58f46cad06eb
Joshua Bell <inexorabletash@gmail.com>
2015-01-27 21:43:00 -0800

    Replace URL params with UI

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
 # elite2trav
 Elite Galaxy to Traveller Sector converter
 
-Load the HTML file with the following URL parameters:
-
-* `gal=N`    for Elite Galaxy 1...8 (default is 1, the home galaxy of Lave)
-* `style=S`
-  * Use 0 for 1 Elite Coordinate Unit = 1 Traveller LY (Elite Galaxy ~= 3 Traveller Sectors; default)
-  * Use 1 for 1 Elite LY = 1 Traveller LY (Elite Galaxy ~= 1/2 Traveller Sector)
+Play with it at: https://inexorabletash.github.io/elite2trav
 
 Elite system generation code is based on [Text
 Elite](http://www.iancgbell.clara.net/elite/text/index.htm) by Ian
@@ -25,7 +20,7 @@ The pseudorandom generation of Elite worlds produces X/Y coordinates
 in the range 0...255. When mapped, the Y coordinate is divided by two,
 and to convert to light years the coordinates are scaled by 0.4. So an
 Elite Galaxy is 102ly by 51ly, or 36 by 17 parsecs on a hex grid -
-slightly more than half a Traveller sector. (parameter: `style=1`)
+slightly more than half a Traveller sector.
 
 This leads to very dense maps, and to maintain trade routes requires
 restricting travel to Jump-2 or about 7ly, comparable with witchspace
@@ -35,7 +30,7 @@ Alternately, the 0.4 scaling factor can be dropped - or, looking at it
 another way, everything can be scaled up by 2.5. When this is done, an
 Elite Galaxy spans just about three Traveller sectors wide and one
 sector tall. Maximum range is Jump-6, and the sparser nature of the
-Elite galaxies is maintained. (parameter: `style=0`)
+Elite galaxies is maintained.
 
 ## About
 
--- jsgal.js
+++ jsgal.js
@@ -1,238 +1,244 @@
-window.onload = function() {
-  var searchParams = (function(url) {
-    var params = {};
-    if (!url.search) return params;
-    url.search.substring(1).split(/&/g).forEach(function(pair) {
-      var kv = pair.split(/=/);
-      params[decodeURIComponent(kv[0])] = decodeURIComponent(kv[1]);
-    });
-    return params;
-  }(window.location));
+document.addEventListener('DOMContentLoaded', function() {
+  var $ = function(s) { return document.querySelector(s); };
 
-  var galnum = 'gal' in searchParams ? searchParams.gal : 0;
-  var style = 'style' in searchParams ? !!(searchParams.style|0) : false;
-
-  // ------------------------------------------------------------------------
-
-  var UNIT_SCALE, HEX_WIDTH, HEX_HEIGHT, JUMP_RANGE;
-
-  if (style) {
-    // 1 Elite LY = 1 Traveller LY
-    // One Elite Galaxy ~= 1/2 Traveller Sector
-    // Range (7 Elite LY) ~= Traveller Jump-2
-    UNIT_SCALE = 0.4;
-    HEX_WIDTH = 36;
-    HEX_HEIGHT = 17;
-    JUMP_RANGE = 2;
-  } else {
-    // 1 Elite Unit = 1 Traveller LY
-    // One Elite Galaxy = 3 Traveller Sectors
-    // Range (7 Elite LY) ~= Traveller Jump-6
-    UNIT_SCALE = 1;
-    HEX_WIDTH = 32 * 3;
-    HEX_HEIGHT = 40;
-    JUMP_RANGE = 6;
-  }
-
-  var PARSEC = 3.2616 / UNIT_SCALE;
-  var PARSEC_SCALE_X = Math.cos(Math.PI / 6);
-  var PARSEC_SCALE_Y = 1;
-
-  function e2t(x, y) {
-    y /= 2;
-    var hx = Math.round(x / PARSEC / PARSEC_SCALE_X + 0.5);
-    var hy = Math.round(y / PARSEC / PARSEC_SCALE_Y - (hx % 2 ? 0 : 0.5) + 1);
-    return { x: hx, y: hy };
-  }
-
-  // ------------------------------------------------------------------------
-
-  // Generate the Elite galaxy, then convert each system to Traveller
-
-  var gal = Galaxy(galnum);
-  gal.forEach(function(system) {
-    system.traveller = elite2traveller(system, e2t);
+  $('#galaxy').addEventListener('change', function() {
+    refresh();
   });
 
-  // ------------------------------------------------------------------------
-
-  var canvas = document.createElement('canvas');
-  document.body.appendChild(canvas);
-
-  var SCALE = 4;
-  canvas.width = 280 * SCALE;
-  canvas.height = 280/2 * SCALE;
-  canvas.style.border = 'solid black 1px';
-  var ctx = canvas.getContext('2d');
-
-  // Background
-  ctx.fillStyle = 'white';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  // Transform to Elite Galaxy scale
-  ctx.scale(SCALE, SCALE);
-  ctx.translate(3, 3);
-  var RADIUS = 0.5;
-
-  // Boundary of Elite Galaxy
-  ctx.lineWidth = 0.25;
-  ctx.strokeStyle = '#ff4040';
-  ctx.strokeRect(0, 0, 256, 256/2);
-
-  function hexToCoords(hx, hy) {
-    hx = hx | 0;
-    hy = hy | 0;
-    return {
-      cx: (hx - 0.5) * PARSEC * PARSEC_SCALE_X,
-      cy: (hy + ((hx % 2) ? 0 : 0.5) - 1) * PARSEC * PARSEC_SCALE_Y
-    };
-  }
-
-  // Jump Routes - background
-  ctx.lineWidth = 4;
-  ctx.strokeStyle = 'rgba(255, 255, 0, 0.25)';
-  ctx.beginPath();
-  forEachPair(gal, function(sys1, sys2) {
-    var hex1 = sys1.traveller.hex;
-    var hex2 = sys2.traveller.hex;
-    var d = dist(hex1, hex2);
-    if (d > JUMP_RANGE) return;
-    var c1 = hexToCoords(hex1 / 100, hex1 % 100);
-    var c2 = hexToCoords(hex2 / 100, hex2 % 100);
-    ctx.moveTo(c1.cx, c1.cy);
-    ctx.lineTo(c2.cx, c2.cy);
+  $('#style').addEventListener('change', function() {
+    refresh();
   });
-  ctx.stroke();
 
-  // Hex Grid
-  for (var x = 1; x <= HEX_WIDTH; ++x) {
-    for (var y = 1; y <= HEX_HEIGHT; ++y) {
-      var c = hexToCoords(x, y);
-      ctx.strokeStyle = '#c0c0c0';
-      ctx.lineWidth = 0.1;
-      ctx.beginPath();
-      ctx.arc(c.cx, c.cy, PARSEC/2*1.1, 0, Math.PI*2);
-      ctx.stroke();
+  refresh();
+
+  function refresh() {
+    var galnum = $('#galaxy').value | 0;
+    var style = !!($('#style').value | 0);
+
+    // ------------------------------------------------------------------------
+
+    var UNIT_SCALE, HEX_WIDTH, HEX_HEIGHT, JUMP_RANGE;
+
+    if (style) {
+      // 1 Elite LY = 1 Traveller LY
+      // One Elite Galaxy ~= 1/2 Traveller Sector
+      // Range (7 Elite LY) ~= Traveller Jump-2
+      UNIT_SCALE = 0.4;
+      HEX_WIDTH = 36;
+      HEX_HEIGHT = 17;
+      JUMP_RANGE = 2;
+    } else {
+      // 1 Elite Unit = 1 Traveller LY
+      // One Elite Galaxy = 3 Traveller Sectors
+      // Range (7 Elite LY) ~= Traveller Jump-6
+      UNIT_SCALE = 1;
+      HEX_WIDTH = 32 * 3;
+      HEX_HEIGHT = 40;
+      JUMP_RANGE = 6;
     }
-  }
 
-  // Jump Routes - foreground
-  ctx.lineWidth = 0.25;
-  ctx.strokeStyle = 'rgba(80, 80, 80, 0.5)';
-  ctx.beginPath();
-  forEachPair(gal, function(sys1, sys2) {
-    var hex1 = sys1.traveller.hex;
-    var hex2 = sys2.traveller.hex;
-    var d = dist(hex1, hex2);
-    if (d > JUMP_RANGE) return;
-    var c1 = hexToCoords(hex1 / 100, hex1 % 100);
-    var c2 = hexToCoords(hex2 / 100, hex2 % 100);
-    ctx.moveTo(c1.cx, c1.cy);
-    ctx.lineTo(c2.cx, c2.cy);
-  });
-  ctx.stroke();
+    var PARSEC = 3.2616 / UNIT_SCALE;
+    var PARSEC_SCALE_X = Math.cos(Math.PI / 6);
+    var PARSEC_SCALE_Y = 1;
 
+    function e2t(x, y) {
+      y /= 2;
+      var hx = Math.round(x / PARSEC / PARSEC_SCALE_X + 0.5);
+      var hy = Math.round(y / PARSEC / PARSEC_SCALE_Y - (hx % 2 ? 0 : 0.5) + 1);
+      return { x: hx, y: hy };
+    }
 
-  // Systems
-  gal.forEach(function(system) {
-    var x = system.x, y = system.y / 2;
+    // ------------------------------------------------------------------------
 
-    var hex = e2t(system.x, system.y);
+    // Generate the Elite galaxy, then convert each system to Traveller
 
-    var c = hexToCoords(hex.x, hex.y);
+    var gal = Galaxy(galnum);
+    gal.forEach(function(system) {
+      system.traveller = elite2traveller(system, e2t);
+    });
 
-    // Line from Elite position to Traveller position
+    // ------------------------------------------------------------------------
+
+    canvas = $('#canvas');
+
+    var SCALE = 4;
+    canvas.width = 280 * SCALE;
+    canvas.height = 280/2 * SCALE;
+    canvas.style.border = 'solid black 1px';
+    var ctx = canvas.getContext('2d');
+
+    // Background
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Transform to Elite Galaxy scale
+    ctx.scale(SCALE, SCALE);
+    ctx.translate(3, 3);
+    var RADIUS = 0.5;
+
+    // Boundary of Elite Galaxy
     ctx.lineWidth = 0.25;
-    ctx.strokeStyle = 'blue';
+    ctx.strokeStyle = '#ff4040';
+    ctx.strokeRect(0, 0, 256, 256/2);
+
+    function hexToCoords(hx, hy) {
+      hx = hx | 0;
+      hy = hy | 0;
+      return {
+        cx: (hx - 0.5) * PARSEC * PARSEC_SCALE_X,
+        cy: (hy + ((hx % 2) ? 0 : 0.5) - 1) * PARSEC * PARSEC_SCALE_Y
+      };
+    }
+
+    // Jump Routes - background
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = 'rgba(255, 255, 0, 0.25)';
     ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(c.cx, c.cy);
+    forEachPair(gal, function(sys1, sys2) {
+      var hex1 = sys1.traveller.hex;
+      var hex2 = sys2.traveller.hex;
+      var d = dist(hex1, hex2);
+      if (d > JUMP_RANGE) return;
+      var c1 = hexToCoords(hex1 / 100, hex1 % 100);
+      var c2 = hexToCoords(hex2 / 100, hex2 % 100);
+      ctx.moveTo(c1.cx, c1.cy);
+      ctx.lineTo(c2.cx, c2.cy);
+    });
     ctx.stroke();
 
-    // Elite position
-    ctx.fillStyle = 'black';
-    ctx.beginPath();
-    ctx.arc(x, y, RADIUS, 0, Math.PI*2);
-    ctx.fill();
-
-    // Traveller position
-    ctx.fillStyle = 'red';
-    ctx.beginPath();
-    ctx.arc(c.cx, c.cy, RADIUS, 0, Math.PI*2);
-    ctx.fill();
-
-    // Name
-    ctx.fillStyle = 'gray';
-    ctx.font = '3px sans-serif';
-    var sz = ctx.measureText(system.name);
-    ctx.fillText(system.name, c.cx - sz.width/2, c.cy + RADIUS + 3);
-  });
-
-  // ------------------------------------------------------------------------
-
-  function forEachPair(array, cbfun) {
-    for (var i = 0; i < array.length; ++i) {
-      for (var j = 0; j < i; ++j) {
-        cbfun(array[i], array[j]);
+    // Hex Grid
+    for (var x = 1; x <= HEX_WIDTH; ++x) {
+      for (var y = 1; y <= HEX_HEIGHT; ++y) {
+        var c = hexToCoords(x, y);
+        ctx.strokeStyle = '#c0c0c0';
+        ctx.lineWidth = 0.1;
+        ctx.beginPath();
+        ctx.arc(c.cx, c.cy, PARSEC/2*1.1, 0, Math.PI*2);
+        ctx.stroke();
       }
     }
+
+    // Jump Routes - foreground
+    ctx.lineWidth = 0.25;
+    ctx.strokeStyle = 'rgba(80, 80, 80, 0.5)';
+    ctx.beginPath();
+    forEachPair(gal, function(sys1, sys2) {
+      var hex1 = sys1.traveller.hex;
+      var hex2 = sys2.traveller.hex;
+      var d = dist(hex1, hex2);
+      if (d > JUMP_RANGE) return;
+      var c1 = hexToCoords(hex1 / 100, hex1 % 100);
+      var c2 = hexToCoords(hex2 / 100, hex2 % 100);
+      ctx.moveTo(c1.cx, c1.cy);
+      ctx.lineTo(c2.cx, c2.cy);
+    });
+    ctx.stroke();
+
+
+    // Systems
+    gal.forEach(function(system) {
+      var x = system.x, y = system.y / 2;
+
+      var hex = e2t(system.x, system.y);
+
+      var c = hexToCoords(hex.x, hex.y);
+
+      // Line from Elite position to Traveller position
+      ctx.lineWidth = 0.25;
+      ctx.strokeStyle = 'blue';
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(c.cx, c.cy);
+      ctx.stroke();
+
+      // Elite position
+      ctx.fillStyle = 'black';
+      ctx.beginPath();
+      ctx.arc(x, y, RADIUS, 0, Math.PI*2);
+      ctx.fill();
+
+      // Traveller position
+      ctx.fillStyle = 'red';
+      ctx.beginPath();
+      ctx.arc(c.cx, c.cy, RADIUS, 0, Math.PI*2);
+      ctx.fill();
+
+      // Name
+      ctx.fillStyle = 'gray';
+      ctx.font = '3px sans-serif';
+      var sz = ctx.measureText(system.name);
+      ctx.fillText(system.name, c.cx - sz.width/2, c.cy + RADIUS + 3);
+    });
+
+    // ------------------------------------------------------------------------
+
+    function forEachPair(array, cbfun) {
+      for (var i = 0; i < array.length; ++i) {
+        for (var j = 0; j < i; ++j) {
+          cbfun(array[i], array[j]);
+        }
+      }
+    }
+
+    // Distance in hexes
+    // dist('0101', '0404') -> 5
+    function dist(a, b) {
+      function even(x) { return (x % 2) == 0; }
+      function odd (x) { return (x % 2) != 0; }
+
+      function div(a, b) { return Math.floor(a / b); }
+      function mod(a, b) { return Math.floor(a % b); }
+
+      a = Number(a);
+      b = Number(b);
+      var a_x = div(a,100);
+      var a_y = mod(a,100);
+      var b_x = div(b,100);
+      var b_y = mod(b,100);
+
+      var dx = b_x - a_x;
+      var dy = b_y - a_y;
+
+      var adx = Math.abs(dx);
+      var ody = dy + div(adx, 2);
+
+      if (odd(a_x) && even(b_x))
+        ody += 1;
+
+      return Math.max(adx - ody, ody, adx);
+    }
+
+    // ------------------------------------------------------------------------
+
+
+    gal.sort(function(a, b) {
+      var ha = a.traveller.hex, hb = b.traveller.hex;
+      return ha < hb ? -1 : ha > hb ? 1 : 0;
+    });
+
+    var out = $('#data');
+    var FORMAT = '%-12s %-4s %-9s %s %-16s %s %s %s %-20s\n';
+    var sec =
+          sprintf(FORMAT,
+                  'Name', 'Hex', 'UWP', 'B', 'Remarks', 'Z', 'PBG', 'Al', 'Stellar') +
+          gal.map(function(system) {
+            var trav = system.traveller;
+            return sprintf(
+              FORMAT,
+              trav.name,
+              trav.hex,
+              trav.uwp,
+              trav.base,
+              trav.remarks,
+              trav.zone,
+              trav.pbg,
+              trav.alleg,
+              trav.stellar
+            );
+          }).join('');
+
+    while (out.firstChild)
+      out.removeChild(out.firstChild);
+    out.appendChild(document.createTextNode(sec));
   }
-
-  // Distance in hexes
-  // dist('0101', '0404') -> 5
-  function dist(a, b) {
-    function even(x) { return (x % 2) == 0; }
-    function odd (x) { return (x % 2) != 0; }
-
-    function div(a, b) { return Math.floor(a / b); }
-    function mod(a, b) { return Math.floor(a % b); }
-
-    a = Number(a);
-    b = Number(b);
-    var a_x = div(a,100);
-    var a_y = mod(a,100);
-    var b_x = div(b,100);
-    var b_y = mod(b,100);
-
-    var dx = b_x - a_x;
-    var dy = b_y - a_y;
-
-    var adx = Math.abs(dx);
-    var ody = dy + div(adx, 2);
-
-    if (odd(a_x) && even(b_x))
-      ody += 1;
-
-    return Math.max(adx - ody, ody, adx);
-  }
-
-  // ------------------------------------------------------------------------
-
-
-  gal.sort(function(a, b) {
-    var ha = a.traveller.hex, hb = b.traveller.hex;
-    return ha < hb ? -1 : ha > hb ? 1 : 0;
-  });
-
-  var out = document.createElement('pre');
-  document.body.appendChild(out);
-  var FORMAT = '%-12s %-4s %-9s %s %-16s %s %s %s %-20s\n';
-  out.appendChild(document.createTextNode(
-    sprintf(FORMAT,
-            'Name', 'Hex', 'UWP', 'B', 'Remarks', 'Z', 'PBG', 'Al', 'Stellar') +
-    gal.map(function(system) {
-      var trav = system.traveller;
-      return sprintf(
-        FORMAT,
-        trav.name,
-        trav.hex,
-        trav.uwp,
-        trav.base,
-        trav.remarks,
-        trav.zone,
-        trav.pbg,
-        trav.alleg,
-        trav.stellar
-      );
-    }).join('')));
-};
+});
